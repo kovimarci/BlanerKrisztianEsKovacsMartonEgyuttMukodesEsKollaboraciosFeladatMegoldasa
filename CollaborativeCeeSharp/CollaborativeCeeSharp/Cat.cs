3f14ba4cc069a293814c9c658ef50a1e0a294f88
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-26 13:43:02 +0100

    Created Cat class file with a summary

---
+++ CollaborativeCeeSharp/CollaborativeCeeSharp/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborativeCeeSharp
+{
+    /// <summary>
+    /// Cat as in Feline
+    /// </summary>
+    internal class Cat
+    {
+    }
+}
