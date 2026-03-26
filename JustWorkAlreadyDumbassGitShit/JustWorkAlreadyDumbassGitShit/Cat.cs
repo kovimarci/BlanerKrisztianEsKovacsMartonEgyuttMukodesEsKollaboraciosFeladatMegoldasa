a9009c2195ddba4eddbfcde98bd89be097c7b78b
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-26 14:12:52 +0100

    Added Summaries to Cat

--- JustWorkAlreadyDumbassGitShit/JustWorkAlreadyDumbassGitShit/Cat.cs
+++ JustWorkAlreadyDumbassGitShit/JustWorkAlreadyDumbassGitShit/Cat.cs
@@ -13,13 +13,19 @@ namespace JustWorkAlreadyDumbassGitShit
     {
         public string Name { get; set; }
         public string Color { get; set; }
-
+        /// <summary>
+        /// Creates an instance of Cat
+        /// </summary>
+        /// <param name="name">the name of the Cat.</param>
+        /// <param name="color">the color of the Cat.</param>
         public Cat(string name, string color)
         {
             Name = name;
             Color = color;
         }
-
+        /// <summary>
+        /// Makes the Cat Meow
+        /// </summary>
         public void Meow()
         {
             Console.WriteLine("Meow");
