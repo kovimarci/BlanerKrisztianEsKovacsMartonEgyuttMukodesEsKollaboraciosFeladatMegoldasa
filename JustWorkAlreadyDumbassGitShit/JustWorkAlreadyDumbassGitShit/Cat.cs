29ef45fa2aba17a423aa980aa323e9ddc61f307d
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-26 14:05:44 +0100

    Added Cat class file with summary

---
+++ JustWorkAlreadyDumbassGitShit/JustWorkAlreadyDumbassGitShit/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustWorkAlreadyDumbassGitShit
+{
+    /// <summary>
+    /// Cat as in Feline
+    /// </summary>
+    internal class Cat
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+    }
+}
